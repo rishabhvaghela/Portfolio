e71a5b726dbb33821ae31cd757e4adcbd7259bf0
Rishabh <vaghelarishu6@gmail.com>
2026-08-08 15:50:49 +0530

    Finilize Mobile-friendly-responsiveness with clean structure and side-bar

--- js/script.js
+++ js/script.js
@@ -1,2 +1,4 @@
-let menubar = document.getElementById('menubar')
-let menu = document.querySelector('.links-group')
+function toggle_menu(){
+    document.querySelector('.side-menu-links').classList.toggle('active')
+    document.querySelector('#side-menu-opener').classList.toggle('active')
+}
